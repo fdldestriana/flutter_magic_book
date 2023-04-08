3cd0a234de717b49564be201f81da6e89e5c373f
fadlydestriana <fadlydestriana@gmail.com>
2023-04-08 23:24:34 +0700

    done creating pageview

--- lib/module/dashboard/controller/dashboard_controller.dart
+++ lib/module/dashboard/controller/dashboard_controller.dart
@@ -3,18 +3,24 @@ import 'package:edashboard2/state_util.dart';
 import '../view/dashboard_view.dart';
 
 class DashboardController extends State<DashboardView>
-    with TickerProviderStateMixin
     implements MvcController {
   static late DashboardController instance;
   late DashboardView view;
-  final PageController pageController = PageController();
-  late TabController tabController;
+  final PageController pageController = PageController(
+    initialPage: 2,
+    viewportFraction: 0.8,
+  );
 
   @override
   void initState() {
     instance = this;
     super.initState();
-    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
   }
 
   @override
--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -1,18 +1,10 @@
 import 'dart:ui';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:edashboard2/core.dart';
 
 class DashboardView extends StatefulWidget {
-  DashboardView({
-    Key? key,
-  }) : super(key: key);
-
-  // final List<String> imageCategories = [
-  //   'https://api.lorem.space/image/watch?w=345&h=180&r=',
-  //   'https://api.lorem.space/image/fashion?w=345&h=180&r=',
-  //   'https://api.lorem.space/image/shoes?w=345&h=180&r=',
-  //   'https://api.lorem.space/image/furniture?w=345&h=180&r=',
-  // ];
+  const DashboardView({super.key});
 
   Widget build(context, DashboardController controller) {
     controller.view = this;
@@ -20,47 +12,38 @@ class DashboardView extends StatefulWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: const EDashboardAppBar(),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 120,
-                child: PageView(
-                  scrollBehavior:
-                      // configuring to swipe pageview with several device
-                      ScrollConfiguration.of(context).copyWith(dragDevices: {
-                    PointerDeviceKind.touch,
-                    PointerDeviceKind.mouse,
-                    PointerDeviceKind.trackpad,
-                  }),
-                  pageSnapping: false,
-                  onPageChanged: (int pageIndex) {
-                    controller.tabController.index = pageIndex;
-                  },
-                  controller: controller.pageController,
-                  children: [
-                    ...List.generate(
-                      4,
-                      (index) => Center(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(6),
-                          child: Image.network(
-                            'https://picsum.photos/id/$index/285/120',
-                            width: 285,
-                            height: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+      body: Column(
+        children: [
+          SizedBox(
+            height: 120,
+            child: PageView.builder(
+              controller: controller.pageController,
+              itemCount: 5,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: SizedBox(
+                    width: 285,
+                    height: double.infinity,
+                    child: Image.network(
+                      'https://picsum.photos/id/$index/285/120',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
                 ),
               ),
-            ],
+              pageSnapping: false,
+              scrollBehavior:
+                  // configuring to swipe pageview with several device
+                  ScrollConfiguration.of(context).copyWith(dragDevices: {
+                PointerDeviceKind.touch,
+                PointerDeviceKind.mouse,
+                PointerDeviceKind.trackpad,
+              }),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
