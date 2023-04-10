4d358943da6745889bf42cb7fa0bfbda9304349f
fadlydestriana <fadlydestriana@gmail.com>
2023-04-11 06:43:53 +0700

    adding height to gridview

--- lib/module/dashboard/widget/product_grid.dart
+++ lib/module/dashboard/widget/product_grid.dart
@@ -18,19 +18,24 @@ class ProductGrid extends StatelessWidget {
             crossAxisSpacing: 15,
             mainAxisSpacing: 20,
             crossAxisCount: 2),
-        itemBuilder: (context, index) => Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(
-                'https://picsum.photos/id/$index/165/135',
-                width: 165,
-                height: 135,
-                fit: BoxFit.cover,
+        itemBuilder: (context, index) => SizedBox(
+          height: 204,
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(
+                  'https://picsum.photos/id/$index/165/135',
+                  width: 165,
+                  height: 135,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const Text('Suede Chukka Boots')
-          ],
+              const Text('Suede Chukka Boots'),
+              const Text('Suede Chukka Boots'),
+              const Text('Suede Chukka Boots'),
+            ],
+          ),
         ),
       ),
     );
